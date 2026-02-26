92ae129c50ce8d533d3218ff756f7d3732321a5e
Evangelos Angelousis <vagelis_agelousis@outlook.com>
2026-02-26 14:54:17 +0200

    Kotlin Multiplatform
    Initialise iOS

--- composeApp/src/iosMain/kotlin/com/agelousis/kotlinmultiplatform/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/agelousis/kotlinmultiplatform/MainViewController.kt
@@ -1,5 +1,16 @@
 package com.agelousis.kotlinmultiplatform
 
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.agelousis.kotlinmultiplatform.expressiveShapes.ui.ExpressiveShapesBaseActivityView
+import com.agelousis.kotlinmultiplatform.expressiveShapes.viewModel.ExpressiveShapesBaseViewModel
+import platform.posix.exit
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    ExpressiveShapesBaseActivityView(
+        viewModel = viewModel { ExpressiveShapesBaseViewModel() },
+        onBackPress = {
+            exit(0)
+        }
+    )
+}
